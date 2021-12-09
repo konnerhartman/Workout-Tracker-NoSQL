ac2c5c61a8a28c4193c087ccee58f3b062711dbd
Konner Hartman <konner.hartman@yahoo.com>
2021-12-09 15:38:14 -0600

    got data rendering in the pounds graph, but showing all workout data instead of last seven days. duration graph is not populating yet.

--- routes/api.js
+++ routes/api.js
@@ -11,4 +11,14 @@ router.get("/api/workouts", (req, res) => {
       });
   });
 
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({})
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+
 module.exports = router;
