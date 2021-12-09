084dacf0ea6ffdeeab72298d74ae4de538d18cdb
Konner Hartman <konner.hartman@yahoo.com>
2021-12-09 15:22:43 -0600

    got last workout to render on homepage

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,14 @@
 const router = require("express").Router();
-const path = require("path");
+const Workout = require("../models/Workout");
 
-
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
 
 module.exports = router;
