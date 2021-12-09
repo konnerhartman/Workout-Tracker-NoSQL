53fdcf533690946b5e63aa1cf33760f891250836
Konner Hartman <konner.hartman@yahoo.com>
2021-12-09 17:37:48 -0600

    fixed code to render most recent seven days data in graph.

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,16 @@
 const router = require("express").Router();
 const Workout = require("../models/Workout");
 
+router.post("/api/workouts", ({ body }, res) => {
+  Workout.create(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
 router.get("/api/workouts", (req, res) => {
     Workout.aggregate([
       {
@@ -16,26 +26,27 @@ router.get("/api/workouts", (req, res) => {
       .catch(err => {
         res.status(400).json(err);
       });
-  });
+});
 
 router.get("/api/workouts/range", (req, res) => {
-    Workout.aggregate([
-      {
-        $addFields: {
-          totalDuration:
-            { $sum: '$exercises.duration' },
-          totalWeight:
-            { $sum: '$exercises.weight' }
-        }
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration:
+          { $sum: '$exercises.duration' },
+        totalWeight:
+          { $sum: '$exercises.weight' }
+      }
     }
   ])
-      .limit(7)
-      .then(dbWorkout => {
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.status(400).json(err);
-      });
-  });
+    .sort({day:-1})
+    .limit(7)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
 
 module.exports = router;
