cdc4ab807581e514b2dcf4a0bec3a42c130a8a82
Konner Hartman <konner.hartman@yahoo.com>
2021-12-09 18:07:24 -0600

    added add new and update routes, but they are not working.

--- routes/api.js
+++ routes/api.js
@@ -11,6 +11,20 @@ router.post("/api/workouts", ({ body }, res) => {
     });
 });
 
+router.put("/api/workouts/:id", (req, res) => {
+  Workout.findByIdAndUpdate(
+    req.params.id,
+    { $push: { exercise: req.body } },
+    { new: true, runValidators: true }
+  )
+  .then(dbWorkout => {
+    res.json(dbWorkout);
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  });
+});
+
 router.get("/api/workouts", (req, res) => {
     Workout.aggregate([
       {
